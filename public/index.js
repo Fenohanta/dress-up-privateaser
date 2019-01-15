788980690ed75c77c730fad1f521f4b479280eb1
Feno <fenohanta@gmail.com>
2019-01-15 16:09:05 +0100

    💦dress-up

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@
     const div = document.createElement('div');
     const template = actors.map(actor => {
       return `
-        <div class="actor">
+        <div class="actor" align="center" ">
           <span>${actor.who}</span>
           <span>${actor.type}</span>
           <span>${actor.amount}</span>
